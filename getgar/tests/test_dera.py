4e42b93f5acaa4e076d9f7623048c1652d2b29bd
Christopher Lo <46541035+topher-lo@users.noreply.github.com>
2020-12-15 05:33:56 +0800

    Fix ordering of rows in dataframe fixtures

--- getgar/tests/test_dera.py
+++ getgar/tests/test_dera.py
@@ -50,14 +50,24 @@ TESTCASES = {
     'process': [
         {'args': (f'{TEST_DATA_PATH}',
                   'txt',
-                  '01-05-2019',
+                  '01-07-2019',
                   '15-12-2019'),
          'expected': pd.DataFrame({
              'adsh': ['0000000001-01-000001',
                       '0000000001-01-000002',
                       '0000000001-01-000003']*2,
-             'dummy_val': ['lorem2019q4', None, 'ipsum2019q4',
-                           'lorem2019q3', None, 'ipsum2019q3']})}
+             'dummy_val': ['lorem2019q3', None, 'ipsum2019q3',
+                           'lorem2019q4', None, 'ipsum2019q4']})},
+        {'args': (f'{TEST_DATA_PATH}',
+                  'txt',
+                  '01-10-2019',
+                  '15-01-2020'),
+         'expected': pd.DataFrame({
+             'adsh': ['0000000001-01-000001',
+                      '0000000001-01-000002',
+                      '0000000001-01-000003']*2,
+             'dummy_val': ['lorem2020q1', None, 'ipsum2020q1',
+                           'lorem2019q4', None, 'ipsum2019q4']})}
     ],
 }
 
@@ -105,6 +115,8 @@ def test_process_sub(process_sub_params):
 
 
 def test_process(process_params):
-    result = process(*process_params[0])
-    expected = process_params[1]
+    result = process(*process_params[0]).sort_values('dummy_val')\
+                                        .reset_index(drop=True)
+    expected = process_params[1].sort_values('dummy_val')\
+                                .reset_index(drop=True)
     assert_frame_equal(result, expected)
