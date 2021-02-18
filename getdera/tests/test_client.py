94d43edc3e82f0b66dd23b4b0c3cc7760551752c
Christopher Lo <46541035+topher-lo@users.noreply.github.com>
2021-02-18 09:24:52 +0800

    Refactor mock responses tests to follow the scrapper's API design

--- getdera/tests/test_client.py
+++ getdera/tests/test_client.py
@@ -10,7 +10,7 @@ from getdera.scrapper.client import _get
 from getdera.scrapper.client import get_DERA
 
 
-### TESTCASES
+# TESTCASES
 
 BASE_URL = "https://www.testingsec.gov/dera/data"
 TEST_ENDPOINT = "testing-data-sets"
@@ -26,7 +26,7 @@ CONTENT = ['1'*3,
            '2'*6,
            '3'*13]
 
-RESPONSES = {
+TEST_RESPONSES = {
     '200': [
         {'url': f'{TEST_URL}/2019q1_rr1.zip',
          'method': 'GET',
@@ -39,7 +39,7 @@ RESPONSES = {
         {'url': f'{TEST_URL}/2019q3_rr1.zip',
          'method': 'GET',
          'body': CONTENT[2],
-         'status': 200}, 
+         'status': 200},
     ],
     '403': {'url': f'{TEST_URL}/2019q1_rr1.zip',
             'method': 'GET',
@@ -47,64 +47,115 @@ RESPONSES = {
     '500': {'url': f'{TEST_URL}/2019q2_rr2.zip',
             'method': 'GET',
             'status': 500},
+    '200_statements': [
+        {'url': f'{TEST_URL}/2020q2_notes.zip',
+         'method': 'GET',
+         'status': 200},
+        {'url': f'{TEST_URL}/2020q3_notes.zip',
+         'method': 'GET',
+         'status': 200},
+        {'url': f'{TEST_URL}/2020_10_notes.zip',
+         'method': 'GET',
+         'status': 200},
+        {'url': f'{TEST_URL}/2020_11_notes.zip',
+         'method': 'GET',
+         'status': 200},
+    ]
 }
 
 TESTCASES = {
     '_get': [
-        {'args': ([r for r in RESPONSES['200']], test_http, 3, 1, 2, 1)},
+        {'kwargs': {'urls': [r['url'] for r in TEST_RESPONSES['200']],
+                    'dir': 'TEMPORARY',
+                    'session': test_http,
+                    'chunk_size': 3,
+                    'timeout': 1,
+                    'retry': 2,
+                    'delay': 1},
+         'expected': [r for r in TEST_RESPONSES['200']]},
     ],
     '_get_error': [
-        {'args': ([RESPONSES['403']], test_http, 3, 1, 2, 1)},
-        {'args': ([RESPONSES['500']], test_http, 3, 1, 2, 1)}
+        {'kwargs': {'urls': [r['url'] for r in TEST_RESPONSES['403']],
+                    'dir': 'TEMPORARY',
+                    'session': test_http,
+                    'chunk_size': 3,
+                    'timeout': 1,
+                    'retry': 2,
+                    'delay': 1},
+         'expected': TEST_RESPONSES['403']},
+        {'kwargs': {'urls': [r['url'] for r in TEST_RESPONSES['500']],
+                    'dir': 'TEMPORARY',
+                    'session': test_http,
+                    'chunk_size': 3,
+                    'timeout': 1,
+                    'retry': 2,
+                    'delay': 1},
+         'expected': TEST_RESPONSES['500']},
     ],
-    'get': [
+    'get_mock': [
+        {'args': ('risk', '01-05-2020', '15-11-2020'),
+         'expected': [r for r in TEST_RESPONSES['200_risk']]},
+        {'args': ('statements', '01-05-2020', '15-11-2020'),
+         'expected': [r for r in TEST_RESPONSES['200_statements']]},
+    ],
+    'get_live': [
         {'args': ('risk', '01-05-2019', '15-12-2019'),
-         'expected': ['2019q2_rr1.zip', '2019q3_rr1.zip']}
+         'expected': ['2019q2_rr1.zip', '2019q3_rr1.zip', '2019q4_rr1.zip']},
     ]
 }
 
 
-### FIXTURES
+# FIXTURES
 
 @pytest.fixture(scope='function', params=TESTCASES['_get'])
 def _get_params(request):
 
-    rsps, *args = request.param['args']
-    urls = [r['url'].split('/')[-1] for r in rsps]
-    return (urls, *args), rsps
+    kwargs = request.param['kwargs']
+    rsps = request.param['expected']
+    return kwargs, rsps
 
 
 @pytest.fixture(scope='function', params=TESTCASES['_get_error'])
 def _get_error_params(request):
 
-    rsps, *args = request.param['args']
-    urls = [r['url'].split('/')[-1] for r in rsps]
-    return (urls, *args), rsps
+    kwargs = request.param['kwargs']
+    rsps = request.param['expected']
+    return kwargs, rsps
 
 
-@pytest.fixture(scope='function', params=TESTCASES['get'])
-def get_params(request):
+@pytest.fixture(scope='function', params=TESTCASES['get_mock'])
+def get_mock_params(request):
+
+    kwargs = request.param['kwargs']
+    rsps = request.param['expected']
+    return kwargs, rsps
+
+
+@pytest.fixture(scope='function', params=TESTCASES['get_live'])
+def get_live_params(request):
 
     dataset, *args = request.param['args']
     expected = request.param['expected']
     return (dataset, *args), expected
 
 
-### UNIT TESTS
+# UNIT TESTS
 
 @responses.activate
 def test_get_(_get_params, tmp_data_directory):
-    """Downloads and saves files from test urls.
+    """Downloads and saves body content from test urls.
     """
-    urls, *req = _get_params[0]
+    kwargs = _get_params[0]
+    # Create tmpdir and set in kwargs
     tmpdir = tmp_data_directory
     utils.make_path(tmpdir)
+    kwargs['dir'] = tmpdir
 
     rsps = _get_params[1]
     contents = [r['body'] for r in rsps]
     for r in rsps:
         responses.add(responses.Response(**r))
-    _get(urls, tmpdir, *req)
+    _get(**kwargs)
 
     saved = []
     for filename in os.listdir(tmpdir):
@@ -121,23 +172,34 @@ def test_get_(_get_params, tmp_data_directory):
 def test_get_error(_get_error_params, tmp_data_directory, caplog):
     """Captures HTTPError and MaxRetryError exceptions.
     """
-
-    urls, *req = _get_error_params[0]
+    kwargs = _get_params[0]
+    # Create tmpdir and set in kwargs
     tmpdir = tmp_data_directory
     utils.make_path(tmpdir)
+    kwargs['dir'] = tmpdir
 
-    rsps = _get_error_params[1][0]
+    rsps = _get_error_params[1]
     responses.add(responses.Response(**rsps))
 
-    _get(urls, tmpdir, *req)
+    _get(**kwargs)
 
     shutil.rmtree(str(tmpdir))
     assert str(rsps['status']) in caplog.text
 
 
+@responses.activate
+def test_get_mock(get_mock_params):
+    """(Mock test) Downloads and saves every relevant DERA
+    dataset between start_date and end_date.
+    """
+
+
+
+
 @pytest.mark.webtest
-def test_get(get_params, tmp_data_directory):
-    """Downloads and saves files from DERA.
+def test_get_live(get_params, tmp_data_directory):
+    """(Live test) Downloads and saves every relevant DERA dataset
+    between start_date and end_date.
     """
 
     dataset, *args = get_params[0]
@@ -147,7 +209,6 @@ def test_get(get_params, tmp_data_directory):
     get_DERA(dataset, tmpdir, *args)
     saved = [os.path.isfile(f'{tmpdir}/{f}') for f in get_params[1]]
 
-    print(saved)
     shutil.rmtree(str(tmpdir))
     assert all(saved)
 
